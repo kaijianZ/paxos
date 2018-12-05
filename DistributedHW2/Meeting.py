b1421f48d22a0b84401f7d8469502c2cd2af5a39
jingfei <jingfei1024@gmail.com>
2018-12-05 18:37:43 -0500

    added paxos and meeting
    renamed original meeting

--- DistributedHW2/Meeting.py
+++ DistributedHW2/Meeting.py
@@ -1,59 +1,129 @@
-from datetime import datetime
+import pickle
+import socket
+import os.path
 
-class Meeting:
-    def __init__(self, name, day,start, end, participants):
+class Meeting(object):
+    def __init__(self, name, date, start, end, participants):
         self.name = name
-        self.day = day
+        self.date = date
         self.start = start
         self.end = end
         self.participants = participants
 
-    def toString(self):
-        nameStr = self.name + " "
-        dayStr = self.day.strftime("%m/%d/%Y ")
-        startStr = self.start.strftime("%H:%M ")
-        endStr = self.end.strftime("%H:%M ")
-        participantStr = ",".join(self.participants)
-        return nameStr + dayStr + startStr + endStr + participantStr
+    def include(self, user):
+        return user in self.participants
 
-#==============================================================================
-#                               Helpers
-#==============================================================================
+    def conflict(self, other_meeting):
+        return other_meeting.date == self.date and \
+               (self.start < other_meeting.end <= self.end or
+                self.start <= other_meeting.start < self.end or
+                other_meeting.start < self.end <= other_meeting.end or
+                other_meeting.start <= self.start < other_meeting.end)
 
-def parseDateString2Datetime(dateStr):
-    return datetime.strptime(dateStr, '%m/%d/%Y')
+    def __str__(self):
+        str_participants = self.participants[0]
+        for user in self.participants[1:]:
+            str_participants += ',' + user
+        return self.name + ' ' + self.date.strftime(
+            "%m/%d/%Y") + ' ' + self.start.isoformat(
+            timespec='minutes') + ' ' + self.end.isoformat(
+            timespec='minutes') + ' ' + str_participants
 
-def parseTimeString2Datetime(timeStr):
-    return datetime.strptime(timeStr, '%H:%M')
 
-def participantStr2List(str):
-    strList = str.split(',')
-    return strList
+class Log(object):
+    def __init__(self, op, value):
+        self.op = op
+        self.value = value
 
-def strList2Meeting(userInputList):
-    name = userInputList[1]
-    day = parseDateString2Datetime(userInputList[2])
-    start = parseTimeString2Datetime(userInputList[3])
-    end = parseTimeString2Datetime(userInputList[4])
-    participants = participantStr2List(userInputList[5])
-    return Meeting( name,
-                    day,
-                    start,
-                    end,
-                    participants)
+    def __str__(self):
+        return self.op + ' ' + str(self.value)
+        
 
-def checkMeetingAvailability(meetingA,meetingB):
-    # both on same day
-    if (meetingA.day == meetingB.day):
-        # whether no overlap between meetings
-        if (meetingA.end <= meetingB.start):
-            # record is earlier than meeting
-            return True
-        elif (meetingB.end <= meetingA.start):
-            # record is later than meeting
-            return True
-        else:
-            return False
+STABLE_STORAGE = 'stable.pkl'
+
+
+def filter_by_participants(calender, user):
+    return list(filter(lambda x: x.include(user), list(calender)))
+
+
+def sorted_view(calender):
+    return sorted(list(calender), key=lambda x: (x.date.strftime("%m/%d/%Y"),
+                                                 x.start.isoformat(
+                                                     timespec='minutes'),
+                                                 x.name))
+
+
+def ok_to_schedule(calender, meeting):
+    participants = meeting.participants
+    for user in participants:
+        schedule = filter_by_participants(calender, user)
+        for s in schedule:
+            if meeting.conflict(s):
+                return False
+    return True
+
+
+def host_to_num(host_list):
+    host_to_num = {}
+    host_list.sort()
+    for i, ele in enumerate(host_list):
+        host_to_num[ele] = i
+    return host_to_num
+
+
+def send_log(matrix_clock, logs, host, port, host_num_dict):
+    host_num = host_num_dict[host]
+    sending_logs = list(filter(lambda rec: not has_rec(matrix_clock, rec,
+                                                       host_num), logs))
+    sending_logs = pickle.dumps((matrix_clock, sending_logs))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.sendto(sending_logs, (host, port))
+    s.close()
+
+
+def has_rec(t, log, host_num):
+    return t[host_num][log.node] >= log.time
+
+
+def notify(participants, this_node, t_i, logs, hosts, host_num_dict):
+    for host in participants:
+        if host != this_node:
+            send_log(t_i, logs, host, hosts[host], host_num_dict)
+
+
+def load_stable(host_len):
+    if os.path.isfile(STABLE_STORAGE) is True:
+        with open(STABLE_STORAGE, 'rb') as fin:
+            obj_list = pickle.load(fin)
+        return obj_list
     else:
-        # no one same day, not possible to have conflict
-        return True
+        t_i = [[0 for _ in range(host_len)] for _ in range(host_len)]
+        calender = {}
+        counter = 0
+        logs = []
+        obj_list = {'t_i': t_i, 'calender': calender,
+                    'counter': counter, 'logs': logs}
+        return obj_list
+
+
+def dump_stable(obj_list):
+    with open(STABLE_STORAGE, 'wb') as fout:
+        pickle.dump(obj_list, fout, pickle.HIGHEST_PROTOCOL)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ DistributedHW2/paxos.py
@@ -0,0 +1,255 @@
+import pickle, math
+import collections
+from meeting import *
+import random
+from threading import Lock, Timer
+lock = RLock()
+
+def getProposeNum(counter, index):
+    return int(str(counter) + str(index))
+    
+    
+class Synod:
+    def __init__(self, logNum, sender, proposeVal: Log, trialNum):
+        self.trialNum = trialNum
+        self.logNum = logNum
+        self.maxPrepare = 0
+        self.counter = 0
+        self.proposeVal = proposeVal
+        self.originProposeVal = proposeVal
+        self.proposeCounter = 0
+        self.accNum = 0
+        self.accVal = None
+        self.accepted = False
+        self.promises = {}
+        self.accepts = collections.Counter()
+        self.sender = sender
+        self.majorityNum = math.ceil(sender.maxIndex/2)
+        
+    def prepare_timeout(self, proposeNum):
+        lock.acquire()
+        
+        if len(self.promises[proposeNum]) < self.majorityNum:
+            self.P_prepare()
+            
+        lock.release()
+            
+    def accept_timeout(self, proposeNum):
+        lock.acquire()
+        
+        if len(self.accepts[msg.accNum]) < self.majorityNum:
+            self.P_prepare()
+            
+        lock.release()
+        
+    def P_prepare(self):
+        lock.acquire()
+        
+        if self.proposeCounter == self.trialNum:
+            self.fail()
+            return
+        
+        self.proposeCounter += 1
+        self.counter += 1
+        proposeNum = getProposeNum(self.counter, self.sender.index)
+        msg = pickle.dumps(Prepare(self.logNum, proposeNum, 
+                                            self.sender.HOSTNAME))
+        self.sender.sendMsgToALL(msg)
+        
+        lock.release()
+        
+        t = Timer(0.05, self.prepare_timeout, [proposeNum])
+        t.start()
+        
+    def A_promise(self, msg: Prepare):
+        lock.acquire()
+        
+        senderHost = msg.senderHost
+        if msg.proposeNum > self.maxPrepare:
+            self.maxPrepare = msg.proposeNum
+            msg = pickle.dumps(Promise(msg.logNum, msg.proposeNum, 
+                  0 if self.accVal is None else self.accNum, self.accVal))
+            self.sender.sendMsg(senderHost, msg)
+        
+        lock.release()
+        
+    def P_request(self, msg: Promise):
+        lock.acquire()
+        
+        self.promises.get(msg.proposeNum, []).append((msg.accNum, msg.accVal))
+        
+        if len(self.promises[msg.proposeNum]) >= self.majorityNum:
+            value = sorted(self.promises[msg.proposeNum], key=lambda x: x[0], 
+                                                            reverse=True)[0][1]
+            
+            self.proposeVal = self.proposeVal if len(list(filter(lambda x: x[1] 
+                   is not None, self.promises[msg.proposeNum]))) == 0 else value
+                   
+            msg = AcptReq(msg.logNum, msg.proposeNum, self.proposeVal, 
+                                                      self.sender.HOSTNAME)
+            msg = pickle.dumps(msg)
+            self.sender.sendMsgToALL(msg)
+        
+        lock.release()
+        t = Timer(0.05, self.accept_timeout, [msg.proposeNum])
+        t.start()
+        
+        
+    def A_accept(self, msg: AcptReq):
+        lock.acquire()
+        
+        senderHost = msg.senderHost
+        if msg.proposeNum >= self.maxPrepare:
+            self.accNum = msg.proposeNum
+            self.accVal = msg.accVal
+            self.maxPrepare = msg.proposeNum
+            msg = Accept(msg.logNum, msg.accNum, msg.accVal)
+            msg = pickle.dumps(msg)
+            self.sender.sendMsg(senderHost, msg)
+
+        lock.release()
+        
+    def P_commit(self, msg: Accept):
+        lock.acquire()
+        
+        self.accepts[msg.accNum] += 1
+        assert(msg.accVal == self.proposeVal)
+        if len(self.accepts[msg.accNum]) >= self.majorityNum:
+            self.accepted = True
+            if self.originProposeVal == self.proposeVal:
+                self.success()
+            msg = pickle.dumps(Commit(self.logNum, msg.accVal))
+            self.sender.sendMsgToALL(msg)
+            
+        lock.release()
+        
+    def success():
+        if self.originProposeVal.op == 'schedule':
+            print('Meeting', self.originProposeVal.value.name, 'scheduled.')
+        else:
+            print('Meeting', self.originProposeVal.value.name, 'cancelled.')
+    
+    def fail():
+        if self.originProposeVal.op == 'schedule':
+            print('Unable to schedule meeting', 
+                            self.originProposeVal.value.name + '.')
+        else:
+            print('Unable to cancel meeting', 
+                            self.originProposeVal.value.name + '.')
+        
+class Paxos:
+    def __init__(self, logSize: int, sender: RadioSend):
+        self.log = [None] * logSize
+        self.logSynod = [None] * logSize
+        self.lastAvailablelogNum = 0
+        self.calender = {} # K: event name, V: event
+        self.sender = sender
+        
+    def view(self):
+        for meeting in sorted_view(calender.values()):
+            print(meeting)
+    
+    def myview(self):
+        for meeting in sorted_view(filter_by_participants(
+                                   calender.values(), this_node)):
+            print(meeting)
+                
+    def addLog(self, msg: Commit):
+        self.log[msg.logNum] = msg.accVal
+        if msg.accVal.op == 'schedule':
+            self.calender[msg.accVal.value.name] = msg.accVal.value
+        else:
+            del self.calender[msg.accVal.value.name]
+        self.lastAvailablelogNum = max(self.lastAvailablelogNum, msg.logNum + 1)
+        
+        
+        
+    def learnVal(self, logNum):
+        if self.logSynod[logNum] is None:
+            self.logSynod[logNum] = Synod(logNum, self.sender, None, 100)
+        
+        self.logSynod[logNum].trialNum = 100
+        self.logSynod[logNum].P_prepare()
+        
+    def recvAccept(self, msg: Accept):
+        self.logSynod[msg.logNum].P_commit(msg)
+        if self.logSynod[msg.logNum].accepted:
+            addLog(Commit(msg.logNum, msg.accVal))
+        
+
+    def msgParser(self, msg):
+        if isinstance(msg, Prepare):
+            self.logSynod[msg.logNum].A_promise(msg)
+        elif isinstance(msg, Promise):
+            self.logSynod[msg.logNum].P_request(msg)
+        elif isinstance(msg, AcptReq):
+            self.logSynod[msg.logNum].A_accept(msg)
+        elif isinstance(msg, Accept):
+            self.recvAccept(msg)
+        elif isinstance(msg, Commit):
+            self.addLog(msg)
+            
+    def insert(self, meeting: Meeting, learn: bool):
+        if self.learnVals(learn):
+            t = Timer(0.1, self.insert, [meeting, False])
+            t.start()
+        else:
+            if ok_to_schedule(self.calender, meeting):
+                logSynod[lastAvailablelogNum] = Synod(lastAvailablelogNum, 
+                        self.sender, Log('schedule', meeting), 3)
+                logSynod[lastAvailablelogNum].P_prepare()
+            else:
+                print('Unable to schedule meeting', meeting.name + '.')
+            
+    def learnVals(self, learn: bool): # return T if hole exists, else F
+        for i in range(self.lastAvailablelogNum):
+            if log[i] is None:
+                if learn:
+                    self.learnVal(i)
+        else:
+            return False
+        return True
+
+    def delete(self, meeting: string, learn: bool):
+        if self.learnVals(learn):
+            t = Timer(0.1, self.delete, [meeting, False])
+            t.start()
+        else:
+            if meeting in self.calender:
+                logSynod[lastAvailablelogNum] = Synod(lastAvailablelogNum, 
+                        self.sender, Log('cancel', meeting), 3)
+                logSynod[lastAvailablelogNum].P_prepare()
+            else:
+                print('Unable to cancel meeting', meeting + '.')
+            
+            
+class Prepare:
+    def __init__(self, logNum, proposeNum, senderHost):
+        self.logNum = logNum
+        self.proposeNum = proposeNum
+        self.senderHost = senderHost
+        
+class Promise:
+    def __init__(self, logNum, proposeNum, accNum, accVal):
+        self.logNum = logNum
+        self.proposeNum = proposeNum
+        self.accNum = accNum
+        self.accVal = accVal
+
+class AcptReq:
+    def __init__(self, logNum, accNum, accVal, senderHost):
+        self.logNum = logNum
+        self.accNum = accNum
+        self.accVal = accVal
+        self.senderHost = senderHost
+        
+class Accept:
+    def __init__(self, logNum, accNum, accVal):
+        self.logNum = logNum
+        self.accNum = accNum
+        self.accVal = accVal
+        
+class Commit:
+    def __init__(self, logNum, accVal):
+        self.logNum = logNum
+        self.accVal = accVal
